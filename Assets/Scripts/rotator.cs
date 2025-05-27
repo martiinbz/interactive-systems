ba0ac6f042ff15b23ea17cdc131bb655cac34b8e
Marc Sirvent <marc.sirvent.ruiz@gmail.com>
2025-05-27 14:02:04 +0200

    Added the environment - and bullets

--- Assets/Scripts/rotator.cs
+++ Assets/Scripts/rotator.cs
@@ -9,6 +9,6 @@ public class rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 30, 30) * Time.deltaTime);
     }
 }
